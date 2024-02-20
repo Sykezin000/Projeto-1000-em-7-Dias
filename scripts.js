861e46b309cd92b4eeef43a4c23d061e55967287
Sykezin000 <thiago08439@hmail.com>
2024-02-19 22:01:56 -0300

    projeto mario

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function  mostrarform(){
+
+
+form.style.left ="50%"
+form.style.transform= "translate(-50%)"
+mascara.style.visibility="visible"
+
+
+
+}
+
+function esconderform(){
+
+    form.style.left ="-300px"
+    form.style.transform= "translate(0)"
+    mascara.style.visibility="hidden"
+
+
+}
+
+
+const form = document.querySelector(".formulario-mario")
+
+
+const mascara = document.querySelector(".mascara-formulario")
+
+
